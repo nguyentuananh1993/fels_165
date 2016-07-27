630c731d389d99c5ca6ccab08e59cc6a2fd46faa
midori-hust <phanthaobk@gmail.com>
2016-07-27 08:30:01 +0700

    fix

---
+++ src/framgiavn/project01/web/business/UserBusiness.java
@@ -0,0 +1,12 @@
+package framgiavn.project01.web.business;
+
+import framgiavn.project01.web.model.User;
+
+public interface UserBusiness {
+	
+	public User findByUserId(Integer user_id) throws Exception;
+
+	public User findByUsername(String username) throws Exception;
+	
+	public void registerUser(User user) throws Exception;
+}
